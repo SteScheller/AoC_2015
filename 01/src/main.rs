0058d806c8698842163435cd3d6130cd74c0d192
Stefan Scheller <ste.scheller@gmail.com>
2023-04-30 13:47:47 +0200

    added solution for part two of day 1

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -16,19 +16,48 @@ fn part_one(input: &str) -> i32 {
 
     result
 }
+
+fn part_two(input: &str) -> usize {
+    let mut floor = 0;
+
+    for (i, c) in input.chars().enumerate() {
+        match c {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => (),
+        }
+        if floor == -1 {
+            return i + 1;
+        }
+    }
+    0
+}
+
 fn main() {
     let input = read_input("input.txt");
     println!("{}", part_one(&input));
+    println!("{}", part_two(&input));
 }
 
-#[test]
-fn test_part_one() {
-    assert!((part_one("(())") == part_one("()()")) && part_one("(())") == 0);
-    assert!(
-        (part_one("(((") == part_one("(()(()("))
-            && (part_one("(((") == part_one("))((((("))
-            && part_one("(((") == 3
-    );
-    assert!((part_one("())") == part_one("))(")) && part_one("())") == -1);
-    assert!((part_one(")))") == part_one(")())())")) && part_one(")))") == -3);
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        assert!((part_one("(())") == part_one("()()")) && part_one("(())") == 0);
+        assert!(
+            (part_one("(((") == part_one("(()(()("))
+                && (part_one("(((") == part_one("))((((("))
+                && part_one("(((") == 3
+        );
+        assert!((part_one("())") == part_one("))(")) && part_one("())") == -1);
+        assert!((part_one(")))") == part_one(")())())")) && part_one(")))") == -3);
+    }
+
+    #[test]
+    fn test_part_two() {
+        assert_eq!(part_two(")"), 1);
+        assert_eq!(part_two("()())"), 5);
+    }
 }
