0f84d35e1d4eacbf44faae58cffc50ec98572f5f
Stefan Scheller <ste.scheller@gmail.com>
2023-04-30 18:54:13 +0200

    added solution for day 2 part 2

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -22,9 +22,29 @@ fn part_one(input: &str) -> u32 {
     result
 }
 
+fn part_two(input: &str) -> u32 {
+    let mut result = 0;
+
+    for line in input.lines() {
+        if let Ok(mut dimensions) = TryInto::<[u32; 3]>::try_into(
+            line.split('x')
+                .map(|x| x.parse::<u32>().unwrap())
+                .collect::<Vec<u32>>(),
+        ) {
+            dimensions.sort();
+            let [a, b, _] = dimensions;
+            result += 2 * (a + b);
+            result += dimensions.iter().fold(1, |acc, x| acc * x);
+        }
+    }
+
+    result
+}
+
 fn main() {
     let input = read_input("input.txt");
     println!("{}", part_one(&input));
+    println!("{}", part_two(&input));
 }
 
 #[cfg(test)]
@@ -35,5 +55,11 @@ mod tests {
     fn test_part_one() {
         assert_eq!(part_one("2x3x4"), 58);
         assert_eq!(part_one("1x1x10"), 43);
+    }
+
+    #[test]
+    fn test_part_two() {
+        assert_eq!(part_two("2x3x4"), 34);
+        assert_eq!(part_two("1x1x10"), 14);
     }
 }
