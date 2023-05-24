94bedf4767854ff621da5f8c4db89e345ddcad7b
Stefan Scheller <ste.scheller@gmail.com>
2023-05-24 22:55:57 +0200

    continued solving day 7

--- 07/src/main.rs
+++ 07/src/main.rs
@@ -1,11 +1,8 @@
 use common;
 use regex::{Captures, Regex};
-use std::{fmt, collections::HashMap};
+use std::{collections::HashMap, fmt};
 
-struct Wire {
-    name: String,
-    signal: u16,
-}
+struct Wire(String);
 
 struct Constant(u16);
 
@@ -20,10 +17,7 @@ impl Argument {
         if let Ok(value) = value {
             Argument::ConstantVariant(Constant(value))
         } else {
-            Argument::WireVariant(Wire {
-                name: s.to_string(),
-                signal: 0,
-            })
+            Argument::WireVariant(Wire(s.to_string()))
         }
     }
 }
@@ -36,6 +30,47 @@ enum Operation {
     RightShift(u8),
     And,
     Or,
+}
+
+impl Operation {
+    pub fn compute(&self, arg1: &Argument, arg2: &Option<Argument>) -> Option<u16> {
+        match &self {
+            Operation::Assignment => match arg1 {
+                Argument::ConstantVariant(c) => Some(c.0),
+                Argument::WireVariant(_) => None,
+            },
+            Operation::Not => match arg1 {
+                Argument::ConstantVariant(c) => Some(!c.0),
+                Argument::WireVariant(_) => None,
+            },
+            Operation::LeftShift(i) => match arg1 {
+                Argument::ConstantVariant(c) => Some(c.0 << i),
+                Argument::WireVariant(_) => None,
+            },
+            Operation::RightShift(i) => match arg1 {
+                Argument::ConstantVariant(c) => Some(c.0 >> i),
+                Argument::WireVariant(_) => None,
+            },
+            Operation::And => {
+                if let (Argument::ConstantVariant(c1), Some(Argument::ConstantVariant(c2))) =
+                    (arg1, arg2)
+                {
+                    Some(c1.0 & c2.0)
+                } else {
+                    None
+                }
+            }
+            Operation::Or => {
+                if let (Argument::ConstantVariant(c1), Some(Argument::ConstantVariant(c2))) =
+                    (arg1, arg2)
+                {
+                    Some(c1.0 | c2.0)
+                } else {
+                    None
+                }
+            }
+        }
+    }
 }
 
 struct Instruction {
@@ -57,20 +92,16 @@ impl fmt::Debug for Instruction {
         };
         let arg1 = match &self.arg1 {
             Argument::ConstantVariant(c) => c.0.to_string(),
-            Argument::WireVariant(w) => w.name.clone(),
-
+            Argument::WireVariant(w) => w.0.clone(),
         };
         let arg2 = match &self.arg2 {
             None => "".to_string(),
-            Some(arg) => {
-                match arg {
-                    Argument::ConstantVariant(c) => c.0.to_string(),
-                    Argument::WireVariant(w) => w.name.clone(),
-                }
-            }
-
+            Some(arg) => match arg {
+                Argument::ConstantVariant(c) => c.0.to_string(),
+                Argument::WireVariant(w) => w.0.clone(),
+            },
         };
-        write!(f, "{} = {arg1}{op}{arg2};", self.out.name)
+        write!(f, "{} = {arg1}{op}{arg2};", self.out.0)
     }
 }
 
@@ -81,28 +112,25 @@ fn get_instructions(input: &str) -> Option<Vec<Instruction>> {
     let re_shift = Regex::new(r"(\w+) (LSHIFT|RSHIFT) (\d+) -> (\w+)").unwrap();
     let re_other = Regex::new(r"(\w+) (AND|OR) (\w+) -> (\w+)").unwrap();
 
-    let capture_to_arg = |c: &Captures, i: usize| {
-        Argument::from_str(c.get(i).unwrap().as_str())
-    };
+    let capture_to_arg = |c: &Captures, i: usize| Argument::from_str(c.get(i).unwrap().as_str());
     let capture_to_wire = |c: &Captures, i: usize| {
         if let Argument::WireVariant(w) = capture_to_arg(c, i) {
             Some(w)
-        }
-        else {
+        } else {
             None
         }
     };
     for line in input.lines() {
         let mut inst = Option::<Instruction>::None;
         if let Some(c) = re_assignment.captures(line) {
-            inst = Some(Instruction{
+            inst = Some(Instruction {
                 op: Operation::Assignment,
                 arg1: capture_to_arg(&c, 1),
                 arg2: None,
                 out: capture_to_wire(&c, 2).unwrap(),
             })
         } else if let Some(c) = re_not.captures(line) {
-            inst = Some(Instruction{
+            inst = Some(Instruction {
                 op: Operation::Not,
                 arg1: capture_to_arg(&c, 2),
                 arg2: None,
@@ -111,10 +139,12 @@ fn get_instructions(input: &str) -> Option<Vec<Instruction>> {
         } else if let Some(c) = re_shift.captures(line) {
             let op = match c.get(2).unwrap().as_str() {
                 "LSHIFT" => Operation::LeftShift(c.get(3).unwrap().as_str().parse::<u8>().unwrap()),
-                "RSHIFT" => Operation::RightShift(c.get(3).unwrap().as_str().parse::<u8>().unwrap()),
+                "RSHIFT" => {
+                    Operation::RightShift(c.get(3).unwrap().as_str().parse::<u8>().unwrap())
+                }
                 _ => panic!("failed to parse shift instruction"),
             };
-            inst = Some(Instruction{
+            inst = Some(Instruction {
                 op: op,
                 arg1: capture_to_arg(&c, 1),
                 arg2: None,
@@ -126,7 +156,7 @@ fn get_instructions(input: &str) -> Option<Vec<Instruction>> {
                 "OR" => Operation::Or,
                 _ => panic!("failed to parse instruction"),
             };
-            inst = Some(Instruction{
+            inst = Some(Instruction {
                 op: op,
                 arg1: capture_to_arg(&c, 1),
                 arg2: Some(capture_to_arg(&c, 3)),
@@ -145,15 +175,51 @@ fn get_instructions(input: &str) -> Option<Vec<Instruction>> {
     }
 }
 
-fn part_one(input: &str) -> HashMap<&str, u16> {
+fn part_one(input: &str) -> HashMap<String, u16> {
     let instructions = get_instructions(input).unwrap();
-    for item in instructions {
-        println!("{:?}", item);
+    let mut results: HashMap<String, u16> = HashMap::new();
+    while results.len() < instructions.len() {
+        for inst in &instructions {
+            if !results.contains_key(&inst.out.0) {
+                let arg1 = results.contains_key(k)
+                let (mut arg1, mut arg2) = (0, 0);
+                match inst.arg1 {
+                    Argument::ConstantVariant(c) => {
+                        arg1 = c.0;
+                    }
+                    Argument::WireVariant(w) => {
+                        if results.contains_key(&w.0) {
+                            arg1 = results.get(&w.0).unwrap().clone();
+                        } else {
+                            continue;
+                        }
+                    }
+                };
+                match inst.arg2 {
+                    None => {}
+                    Some(arg) => match arg {
+                        Argument::ConstantVariant(c) => {
+                            arg2 = c.0;
+                        }
+                        Argument::WireVariant(w) => {
+                            if results.contains_key(&w.0) {
+                                arg2 = results.get(&w.0).unwrap().clone();
+                            } else {
+                                continue;
+                            }
+                        }
+                    },
+                };
+                if let Some(value) = inst.op.compute(arg1, arg2) {
+                    results.insert(inst.out.0.clone(), value);
+                }
+            }
+        }
     }
-    HashMap::new()
+    results
 }
 
-fn part_two(input: &str) -> HashMap<&str, u16> {
+fn part_two(_input: &str) -> HashMap<String, u16> {
     //let instructions = get_instructions(input).unwrap();
     HashMap::new()
 }
@@ -171,14 +237,14 @@ mod tests {
     #[test]
     fn test_part_one() {
         let expected_map = HashMap::from([
-            ("d", 72),
-            ("e", 507),
-            ("f", 492),
-            ("g", 114),
-            ("h", 65412),
-            ("i", 65079),
-            ("x", 123),
-            ("y", 456),
+            ("d".to_string(), 72),
+            ("e".to_string(), 507),
+            ("f".to_string(), 492),
+            ("g".to_string(), 114),
+            ("h".to_string(), 65412),
+            ("i".to_string(), 65079),
+            ("x".to_string(), 123),
+            ("y".to_string(), 456),
         ]);
         let computed_map = part_one(
             "123 -> x
