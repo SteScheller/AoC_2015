34eff754a69f5d2bab522a1b4af4859fb835bee6
Stefan Scheller <ste.scheller@gmail.com>
2024-03-29 21:25:20 +0100

    added solution for day 17 part 2

--- 17/src/main.rs
+++ 17/src/main.rs
@@ -1,4 +1,4 @@
-use std::cmp::Ordering;
+use std::cmp::{min, Ordering};
 
 fn get_containers(input: &str) -> Vec<u32> {
     let mut containers = Vec::new();
@@ -26,13 +26,67 @@ fn find_combinations(remaining: i32, summands: &[u32]) -> u32 {
     }
 }
 
+fn find_min_num_containers(remaining: i32, summands: &[u32], acc: u32) -> Option<u32> {
+    match remaining.cmp(&0) {
+        Ordering::Less => None,
+        Ordering::Equal => Some(acc),
+        Ordering::Greater => {
+            if summands.is_empty() {
+                None
+            } else {
+                let with = find_min_num_containers(
+                    remaining - summands[0] as i32,
+                    &summands[1..],
+                    acc + 1,
+                );
+                let without = find_min_num_containers(remaining, &summands[1..], acc);
+                match (with, without) {
+                    (Some(a), Some(b)) => Some(min(a, b)),
+                    (Some(a), None) => Some(a),
+                    (None, Some(b)) => Some(b),
+                    (None, None) => None,
+                }
+            }
+        }
+    }
+}
+
+fn find_min_combinations(remaining: i32, summands: &[u32], acc: u32, min_num: u32) -> u32 {
+    match remaining.cmp(&0) {
+        Ordering::Less => 0,
+        Ordering::Equal => {
+            if acc <= min_num {
+                1
+            } else {
+                0
+            }
+        }
+        Ordering::Greater => {
+            if summands.is_empty() {
+                0
+            } else {
+                let with = find_min_combinations(
+                    remaining - summands[0] as i32,
+                    &summands[1..],
+                    acc + 1,
+                    min_num,
+                );
+                let without = find_min_combinations(remaining, &summands[1..], acc, min_num);
+                with + without
+            }
+        }
+    }
+}
+
 fn part_one(input: &str, sum_eggnog: u32) -> u32 {
     let containers = get_containers(input);
     find_combinations(sum_eggnog as i32, &containers)
 }
 
-fn part_two(_input: &str, _sum_eggnog: u32) -> u32 {
-    0
+fn part_two(input: &str, sum_eggnog: u32) -> u32 {
+    let containers = get_containers(input);
+    let min = find_min_num_containers(sum_eggnog as i32, &containers, 0).unwrap();
+    find_min_combinations(sum_eggnog as i32, &containers, 0, min)
 }
 
 fn main() {
@@ -47,6 +101,11 @@ mod tests {
 
     #[test]
     fn test_find_combinations() {
-        assert_eq!(find_combinations(25, &[20, 15, 10, 5, 5]), 4)
+        assert_eq!(find_combinations(25, &[20, 15, 10, 5, 5]), 4);
+    }
+
+    #[test]
+    fn test_find_min_num_containers() {
+        assert_eq!(find_min_num_containers(25, &[20, 15, 10, 5, 5], 0), Some(2));
     }
 }
