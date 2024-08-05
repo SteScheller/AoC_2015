a687bbc543c67531443910a64bdb82b95b148335
Stefan Scheller <stefan.scheller@endress.com>
2024-08-05 08:43:03 +0200

    add input parsing for day 18 part one

--- 18/src/main.rs
+++ 18/src/main.rs
@@ -1,5 +1,50 @@
-fn part_one(input: &str) -> usize {
-    0
+struct LightGrid {
+    width: usize,
+    height: usize,
+    arr: Vec<Vec<bool>>,
+}
+
+impl LightGrid {
+    pub fn new(width: usize, height: usize) -> Self {
+        let arr = vec![vec![false; height]; width];
+        Self { width, height, arr }
+    }
+
+    pub fn from_str(input: &str) -> Self {
+        let mut width = 0;
+        let mut height = 0;
+        let mut arr = Vec::new();
+        for line in input.lines() {
+            width = line.len();
+            height += 1;
+            let mut row = Vec::new();
+            for c in line.chars() {
+                row.push(c == '#');
+            }
+            arr.push(row);
+        }
+        Self { width, height, arr }
+    }
+
+    pub fn get_num_switched_on(&self) -> usize {
+        self.arr.iter().flatten().filter(|x| **x).count()
+    }
+
+    fn simulate_step(&mut self) -> () {
+        let current = self.arr.clone();
+    }
+
+    pub fn simulate_steps(&mut self, steps: usize) -> () {
+        for _ in 0..steps {
+            self.simulate_step();
+        }
+    }
+}
+
+fn part_one(input: &str, steps: usize) -> usize {
+    let mut grid = LightGrid::from_str(input);
+    grid.simulate_steps(steps);
+    grid.get_num_switched_on()
 }
 
 fn part_two(input: &str) -> u32 {
@@ -8,7 +53,7 @@ fn part_two(input: &str) -> u32 {
 
 fn main() {
     let input = common::read_input("input.txt");
-    println!("{}", part_one(&input));
+    println!("{}", part_one(&input, 100));
     println!("{}", part_two(&input));
 }
 
@@ -17,12 +62,36 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_light_grid_construction() {
+        let grid = LightGrid::from_str(
+            ".#.#.#\n\
+            ...##.\n\
+            #....#\n\
+            ..#...\n\
+            #.#..#\n\
+            ####.."
+        );
+        assert_eq!(grid.get_num_switched_on(), 15);
+    }
+
+    #[test]
     fn test_part_one() {
-        assert_eq!(part_one("2,3,0,3,10,11,12,1,1,0,1,99,2,1,1,2"), 4);
+        assert_eq!(
+            part_one(
+                ".#.#.#\n\
+                ...##.\n\
+                #....#\n\
+                ..#...\n\
+                #.#..#\n\
+                ####..",
+                4
+            ),
+            4
+        );
     }
 
     #[test]
     fn test_part_two() {
-        assert_eq!(part_one("2,3,0,3,10,11,12,1,1,0,1,99,2,1,1,2"), 0);
+        assert_eq!(part_two("2,3,0,3,10,11,12,1,1,0,1,99,2,1,1,2"), 4);
     }
 }
